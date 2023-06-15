2aa2ae825728b2c9460e083d8d75e75bbf117473
Only1Denno <only1denno@gmail.com>
2023-06-15 11:47:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * *main -A c program that print a line to the satndard error
- * *Return 1 (Success)
- * */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	fput("and that palce of art is useful\" - Dora Korpar, 2015-10-19\r" stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
