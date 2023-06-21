6e2384d11ec9996adeaf771ff10b6760dc41eae6
Only1Denno <only1denno@gmail.com>
2023-06-21 08:33:33 -0700

    101-natural

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,18 @@
-#include "main.h"
-/**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
- */
-void print_times_table(int n)
-{
-	int i, j, res;
+#include <stdio.h>
 
-	if (!(n > 15 || n < 0))
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+void times_table(int n);
+
+int main() {
+    int limit = 10;
+    times_table(limit);
+
+    return 0;
 }
+
+void times_table(int n) {
+    for (int i = 0; i <= n; i++) {
+        int result = 9 * i;
+        printf("9 x %d = %d\n", i, result);
+    }
+}
+
