8b17cfa73642afafc29ba5098d70d2c784cdeeaf
Only1Denno <only1denno@gmail.com>
2023-06-26 06:49:22 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,31 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 8
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int pass[100];
+int i, sum, n;
 
-char generateRandomCharacter() {
-    // Generates a random character from the set of alphanumeric characters
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int index = rand() % (sizeof(charset) - 1);
-    return charset[index];
+sum = 0;
+
+srand(time(NULL));
+
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum)-'0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
 }
 
-void generateRandomPassword(char *password) {
-    // Generates a random password of the specified length
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = generateRandomCharacter();
-    }
-    password[PASSWORD_LENGTH] = '\0';
-}
-
-int main() {
-    srand(time(NULL)); // Seed the random number generator with the current time
-
-    char password[PASSWORD_LENGTH + 1];
-    generateRandomPassword(password);
-
-    printf("Generated Password: %s\n", password);
-
-    return 0;
+return (0);
 }
 
